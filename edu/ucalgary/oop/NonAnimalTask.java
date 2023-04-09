532ea5cf2dc2218651ba9ccb79a9ad8382b0062f
David-Rodriguez-Barrios <david.rodriguezbarri@ucalgary.ca>
2023-04-08 18:48:08 -0600

    commented out files we dont need

--- edu/ucalgary/oop/NonAnimalTask.java
+++ edu/ucalgary/oop/NonAnimalTask.java
@@ -1,31 +1,31 @@
-package edu.ucalgary.oop;
+// package edu.ucalgary.oop;
 
-public class NonAnimalTask extends Task {
-    private int cage;
-    private int feeding;
+// public class NonAnimalTask extends Task {
+//     private int cage;
+//     private int feeding;
 
-    // Constructor
-    public NonAnimalTask(int cage, int feeding, int startHour, int taskID, int maxWindow) {
-        super(startHour, taskID, maxWindow);
-        setCage(cage);
-        setFeeding(feeding);
-    }
+//     // Constructor
+//     public NonAnimalTask(int cage, int feeding, int startHour, int taskID, int maxWindow) {
+//         super(startHour, taskID, maxWindow);
+//         setCage(cage);
+//         setFeeding(feeding);
+//     }
 
-    // Getters and setters
-    public void setCage(int cage) {
-        this.cage = cage;
-    }
+//     // Getters and setters
+//     public void setCage(int cage) {
+//         this.cage = cage;
+//     }
 
-    public void setFeeding(int feeding) {
-        this.feeding = feeding;
-    }
+//     public void setFeeding(int feeding) {
+//         this.feeding = feeding;
+//     }
 
-    public int getCage() {
-        return this.cage;
-    }
+//     public int getCage() {
+//         return this.cage;
+//     }
 
-    public int getFeeding() {
-        return this.feeding;
-    }
+//     public int getFeeding() {
+//         return this.feeding;
+//     }
 
-}
+// }
