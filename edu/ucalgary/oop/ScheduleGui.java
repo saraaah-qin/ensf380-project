156355f8a97f6f35b45079097a92bc0791ca329b
AidanW86 <aidan.wong1@ucalgary.ca>
2023-04-10 17:14:35 -0600

    updated gui for backup & start of modify

--- edu/ucalgary/oop/ScheduleGui.java
+++ edu/ucalgary/oop/ScheduleGui.java
@@ -7,13 +7,17 @@ import java.awt.event.ActionEvent;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class ScheduleGui extends JFrame implements ActionListener {
     private JLabel label;
     private JButton button;
+    private JButton modifybutton;
     private Schedule schedule;
     private JTextArea resultTextArea;
+    private JTextArea modifyTextArea;
     private JScrollPane scrollPane;
+    private JScrollPane modscrollPane;
 
     public ScheduleGui() {
         setTitle("Generate Schedule");
@@ -23,16 +27,22 @@ public class ScheduleGui extends JFrame implements ActionListener {
 
         label = new JLabel("Generate Schedule");
         button = new JButton("Generate Schedule");
+        modifybutton = new JButton("Modify Schedule");
+        modifyTextArea = new JTextArea(5, 15);
         resultTextArea = new JTextArea(50, 50);
         scrollPane = new JScrollPane(resultTextArea);
+        modscrollPane = new JScrollPane(modifyTextArea);
 
         setLayout(new FlowLayout());
 
         add(label);
         add(button);
         add(scrollPane);
+        add(modscrollPane);
+        add(modifybutton);
 
         button.addActionListener(this);
+        modifybutton.addActionListener(this);
         schedule = new Schedule();
     }
 
@@ -40,15 +50,46 @@ public class ScheduleGui extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == button) {
             String result = schedule.generateSchedule();
-            JOptionPane.showMessageDialog(this, "Schedule generated");
-            resultTextArea.setText(result);
-            System.out.println(result);
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter("schedule.txt"))) {
-                writer.write(result);
-                writer.newLine();
-            } catch (IOException ex) {
-                ex.printStackTrace();
+            Scanner search = new Scanner(result);
+            String msg = "";
+            boolean backup = false;
+            while(search.hasNextLine()){
+                String line = search.nextLine();
+                if(line.contains("[+ backup volunteer]")){
+                    backup = true;
+                    String[] data = line.split("\\s+");
+                    msg += data[0] + " ";
+                }
             }
+            if(backup){
+                String warning = "Confirm backups have been contacted for these times: " + msg;
+                JOptionPane.showMessageDialog(this, warning);
+            }
+
+            // need to implement modifying the start hour of one or more treatments based on advice she receives
+            if(result.startsWith("No schedule possible")){
+                String modmsg = "Schedule is not possible, contact staff vet";
+                JOptionPane.showMessageDialog(this, modmsg);
+                resultTextArea.setText(result);
+                System.out.println(result);
+            }
+            else{
+                JOptionPane.showMessageDialog(this, "Schedule generated");
+                resultTextArea.setText(result);
+
+                System.out.println(result);
+
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter("schedule.txt"))) {
+                    writer.write(result);
+                    writer.newLine();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+        else if(e.getSource() == modifybutton){
+
+            // MARKER HERE // 
         }
     }
 
